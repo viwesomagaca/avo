a17b17b96b844d85af253d24cb9266384a669379
viwesomagaca <viwesomagaca29@gmail.com>
2017-05-10 11:00:51 +0200

    my initial Commit

---
+++ avos.js
@@ -0,0 +1,11 @@
+ function bestDeal(offers){
+	var price = offers.split(",");
+	 	 var newList = [];
+	 
+	 for (var i= 0 i< price.length; i++){
+	   newList.push(price[i]);
+	  
+	 }
+	 
+ }
+ return newList;
---
+++ test/avos.test.js
@@ -0,0 +1,6 @@
+describe("The best of deals"), function(){
+	it("should return the best place to buy Avocados!", function(){
+		
+	assert.equal(bestDeal("1 for R3, 2 for R7, 3 for R10, 5 for R14.50."))	
+	})
+})
